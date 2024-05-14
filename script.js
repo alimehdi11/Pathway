a7b3aaff549874d4f1d8337096fcdf456d804717
ali mehdi <ykhizar2@gmail.com>
2024-05-14 16:30:24 +0500

    s

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//get elements
+let menuBtn = document.querySelector(".menu");
+let sideBar = document.querySelector(".sidebar");
+let overlay = document.querySelector(".overlay");
+
+
+//functions definations
+const handleSidebar = () =>{
+  sideBar.classList.toggle("active")
+  overlay.classList.toggle("active")
+};
+
+
